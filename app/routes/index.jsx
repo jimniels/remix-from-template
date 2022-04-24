24a944d6ca44c0c1863dd8fb6dd5c0e1d62dbba1
Jim Nielsen <jimniels@gmail.com>
2022-04-24 13:39:45 -0600

    first commit

---
+++ app/routes/index.jsx
@@ -0,0 +1,42 @@
+import { useLoaderData, useActionData } from "@remix-run/react";
+import { json } from "@remix-run/server-runtime";
+
+export async function action({ request }) {
+  const body = await request.formData();
+  return json({ title: "Test" });
+}
+
+export default function Index() {
+  const actionData = useActionData();
+  console.log(actionData);
+  return (
+    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
+      <h1>Welcome to Remix</h1>
+      <ul>
+        <li>
+          <a
+            target="_blank"
+            href="https://remix.run/tutorials/blog"
+            rel="noreferrer"
+          >
+            15m Quickstart Blog Tutorial
+          </a>
+        </li>
+        <li>
+          <a
+            target="_blank"
+            href="https://remix.run/tutorials/jokes"
+            rel="noreferrer"
+          >
+            Deep Dive Jokes App Tutorial
+          </a>
+        </li>
+        <li>
+          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
+            Remix Docs
+          </a>
+        </li>
+      </ul>
+    </div>
+  );
+}
---
+++ remix.config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('@remix-run/dev').AppConfig}
+ */
+module.exports = {
+  "serverBuildTarget": "netlify-edge",
+  "server": "./server.js",
+  "ignoredRouteFiles": [
+    ".*"
+  ]
+}
